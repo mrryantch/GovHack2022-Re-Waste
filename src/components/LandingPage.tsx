ab9428e30b9dfddaa20505bcf21a5a324a48863f
piska0921 <negeromu@163.com>
2022-08-20 18:43:23 +0930

    router

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Header = () => {
+    return <>
+    <div>re:waste</div>
+    <a href="#">Link1</a>
+    <a href="#">Link2</a>
+    <a href="#">Link3</a>
+    <a href="#">Link4</a>
+    <button>CTA</button>
+    </>
+}
+
+const LandingPage = () => {
+
+    return <>
+    <Header />
+    <h1>This is a landing page</h1>
+    </>
+}
+
+export default LandingPage
