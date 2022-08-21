dfd8e3cb625e48572291848234fd1ed6a210b968
Ryan <mrryantch@gmail.com>
2022-08-21 16:48:41 +0930

    fix dashboard button

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -15,80 +15,96 @@ const NavContainer = styled.div`
   @media (max-width: 768px) {
     flex-direction: column;
     padding: 0;
-}
-`
+  }
+`;
 
 const ButtonsContainer = styled.div`
-    display: flex;
-    padding-top: 1.5rem;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  padding-top: 1.5rem;
+  justify-content: center;
+  align-items: center;
 
-    @media (max-width: 768px) {
-        flex-direction: column;
-    }
-`
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
 
 const MultiLingualSelect = styled.div`
-    display: flex;
-    padding-left: 2rem;
-    @media (max-width: 768px) {
-        padding: 1rem 0;
-    }
-    justify-content: center;
-    align-items: center;
-`
+  display: flex;
+  padding-left: 2rem;
+  @media (max-width: 768px) {
+    padding: 1rem 0;
+  }
+  justify-content: center;
+  align-items: center;
+`;
 
 interface LanguageOptionProps {
-    selected?: boolean;
+  selected?: boolean;
 }
 
 const LanguageOption = styled.span<LanguageOptionProps>`
-    padding: 0 0.5rem;
-    color: ${props => props.selected ? GREEN : BLACK};
+  padding: 0 0.5rem;
+  color: ${(props) => (props.selected ? GREEN : BLACK)};
 
-    &:hover {
-        cursor: pointer;
-        color: ${GREEN};
-        opcacity: 0.8;
-    }
-
-`
+  &:hover {
+    cursor: pointer;
+    color: ${GREEN};
+    opcacity: 0.8;
+  }
+`;
 
 interface HeaderProps {
-    language?: number;
-    setLanguage?: (language: number) => void
+  language?: number;
+  setLanguage?: (language: number) => void;
 }
 
 export const Header = (props: HeaderProps) => {
-    const navigate = useNavigate();
-    const {
-        language = LanguageType.English, 
-        setLanguage
-    } = props;
+  const navigate = useNavigate();
+  const { language = LanguageType.English, setLanguage } = props;
 
-    const onDashboardClick = () => {
-        if (window.sessionStorage.getItem("user")) {
-            navigate("/dashboard");
-        } else {
-            navigate("/login");
-        }
-    };
+  const onDashboardClick = () => {
+    if (window.sessionStorage.getItem("user")) {
+      navigate("/dashboard");
+    } else {
+      navigate("/login");
+    }
+  };
 
-    return (
-        <NavContainer>
-            <HeaderText>
-                re:<GreenSpan>waste</GreenSpan>
-            </HeaderText>
-            <ButtonsContainer>
-                <LinkButton href="/code-entry">Dispose Your Waste</LinkButton>
-                <LinkButton href="/">Home Page</LinkButton>
-                <PrimaryButton onClick={onDashboardClick} style={{ maxHeight: '2rem' }}>Dashboard</PrimaryButton>
-                <MultiLingualSelect>
-                    <LanguageOption selected={language === LanguageType.English} onClick={() => setLanguage && setLanguage(LanguageType.English)}>English</LanguageOption> |
-                    <LanguageOption selected={language === LanguageType.Chinese} onClick={() => setLanguage && setLanguage(LanguageType.Chinese)}>简体中文</LanguageOption>
-                </MultiLingualSelect>
-            </ButtonsContainer>
-        </NavContainer>
-    );
+  return (
+    <NavContainer>
+      <HeaderText>
+        re:<GreenSpan>waste</GreenSpan>
+      </HeaderText>
+      <ButtonsContainer>
+        <LinkButton href="/code-entry">Dispose Your Waste</LinkButton>
+        <LinkButton href="/">Home Page</LinkButton>
+        <PrimaryButton
+          onClick={onDashboardClick}
+          style={{
+            maxHeight: "2rem",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          Dashboard
+        </PrimaryButton>
+        <MultiLingualSelect>
+          <LanguageOption
+            selected={language === LanguageType.English}
+            onClick={() => setLanguage && setLanguage(LanguageType.English)}
+          >
+            English
+          </LanguageOption>{" "}
+          |
+          <LanguageOption
+            selected={language === LanguageType.Chinese}
+            onClick={() => setLanguage && setLanguage(LanguageType.Chinese)}
+          >
+            简体中文
+          </LanguageOption>
+        </MultiLingualSelect>
+      </ButtonsContainer>
+    </NavContainer>
+  );
 };
